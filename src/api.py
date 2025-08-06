25127b41fd9fe1088551bc30081ef1559d7a8a8a
Bruno Coulet <bruno.coulet@laplateforme.io>
2025-08-06 10:17:18 +0200

    Update api.py

--- src/api.py
+++ src/api.py
@@ -5,54 +5,17 @@ from database import insert_questions
 
 load_dotenv()
 COLLECTION = os.getenv("COLLECTION_API")
-PAGESIZE = 100
 
-def get_questions(PAGESIZE):
-    url = "https://api.stackexchange.com/2.3/questions"
-    tags = "data-science"
-    params = {
-        "order": "desc",
-        "sort": "creation",
-        "site": "stackoverflow",
-        "pagesize": PAGESIZE,
-        "tagged": tags,
-        # # 01/01/2025
-        # "fromdate": 1735686000,
-        # # 05/08/2025
-        # "todate": 1754344800
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
-    questions = []
-    for item in data.get("items", []):
-        questions.append({
-            "title": item["title"],
-            "link": item["link"],
-            "tags": item["tags"],
-            "author": item["owner"].get("display_name"),
-            "date": item["creation_date"]
-        })
-    return questions
-
-def get_questions_paginated():
-    """
-    Récupère jusqu'à 1000 questions en utilisant la pagination (10 pages de 100 questions).
-    """
-    # tags = "python;r;sql;julia;r-language;scala;java;matlab;excel;sas;tensorflow;pytorch;keras;pandas;numpy;scikit-learn;xgboost;lightgbm;nlp"
+def get_datascience_questions(page_size: int = 100, nb_pages: int = 2):
     url = "https://api.stackexchange.com/2.3/questions"
     questions = []
-    for page in range(1, 11):
+    for page in range(1, nb_pages + 1):
         params = {
         "order": "desc",
         "sort": "creation",
         "site": "stackoverflow",
-        "pagesize": PAGESIZE,
+        "pagesize": page_size,
         "tagged": "data-science",
-
-        # # 01/01/2025
-        # "fromdate": 1735686000,
-        # # 05/08/2025
-        # "todate": 1754344800,
         "page": page
         }
         response = requests.get(url, params=params)
@@ -70,7 +33,7 @@ def get_questions_paginated():
 
 
 if __name__ == "__main__":
-    test = get_questions_paginated()
+    test = get_datascience_questions()
     for q in test:
         print(q)
-    insert_questions(test, COLLECTION, append=False)
+    insert_questions(test, COLLECTION, append=True)
