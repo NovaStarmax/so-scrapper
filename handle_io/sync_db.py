edd16371d1ceb8240c09177991f2259e9c7a4ee6
Bruno Coulet <bruno.coulet@laplateforme.io>
2025-08-06 12:31:59 +0200

    sync bug

--- handle_io/sync_db.py
+++ handle_io/sync_db.py
@@ -1,19 +1,11 @@
 from pymongo import MongoClient
 import json, os
-from database import get_collection
+from src.database import get_collection
 from dotenv import load_dotenv
 
-load_dotenv()
 
-COLLECTION_API = os.getenv("COLLECTION_API")
-COLLECTION_SCRAPPER = os.getenv("COLLECTION_SCRAPPER")
-
-
-
-
-
-def append_new_documents_to_json(json, collection):
-    json_path = "handle_io/" + json
+def append_new_documents_to_json(json_file, collection):
+    json_path = "handle_io/" + json_file
     # Charger le fichier JSON s'il existe
     try:
         with open(json_path, "r", encoding="utf-8") as f:
@@ -43,5 +35,29 @@ def append_new_documents_to_json(json, collection):
     else:
         print("Aucun nouveau document à ajouter.")
 
-# Appel direct (optionnel)
-# append_new_documents_to_json()
+
+def import_data(json_file, collection):
+    json_path = "handle_io/" + json_file
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    added_count = 0
+
+    # Récupérer tous les liens existants en base
+    existing_links = set(doc["link"] for doc in collection.find({}, {"link": 1}))
+
+    # Ajouter uniquement les nouveaux documents
+    for doc in data:
+        if "link" not in doc:
+            continue  # ignorer les documents sans champ "link"
+        if doc["link"] not in existing_links:
+            collection.insert_one(doc)
+            added_count += 1
+            existing_links.add(doc["link"])  # éviter les doublons dans la même session
+
+    print(f"{added_count} nouveaux documents insérés.")
+
+
+def synchronize(json_file: str, collection):
+    append_new_documents_to_json(json_file, collection)
+    import_data(json_file, collection)
--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import os
 # sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 # sys.path.append(os.path.join(os.path.dirname(__file__), "handle_io"))
 from dotenv import load_dotenv
-from database import get_collection
-from handle_io.export import append_new_documents_to_json
-
-
+from src.database import get_collection
+from handle_io.sync_db import synchronize
 
 load_dotenv()
 COLLECTION_API = os.getenv("COLLECTION_API")
@@ -20,5 +18,6 @@ def setup_indexes(collection: str):
 if __name__ == "__main__":
     # setup_indexes(COLLECTION_API)
     # setup_indexes(COLLECTION_SCRAPPER)
-    append_new_documents_to_json("api.json", get_collection(COLLECTION_API))
+    # synchronize("api.json", get_collection(COLLECTION_API))
+    synchronize("scrapper.json", get_collection(COLLECTION_SCRAPPER))
     # append_new_documents_to_json("scrapper.json", get_collection(COLLECTION_SCRAPPER))
